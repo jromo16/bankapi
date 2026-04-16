4976dc6648bfe061b64e685b2e198282deaa852f
Jhon Romo <jcrr1611@gmail.com>
2026-04-16 10:05:58 -0500

    se agregan archivos

--- src/main/java/com/bankapi/config/SecurityConfig.java
+++ src/main/java/com/bankapi/config/SecurityConfig.java
@@ -27,7 +27,8 @@ public class SecurityConfig {
             "/swagger-ui/**",
             "/swagger-ui.html",
             "/v3/api-docs/**",
-            "/h2-console/**"
+            "/h2-console/**",
+            "/actuator/health"
     };
 
     @Bean
